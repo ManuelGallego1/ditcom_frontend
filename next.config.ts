2a46f3dfe0b3ad0f5463a1487dc85877d63f5e4b
ManuelGallego1 <manuela.gallegoa@autonoma.edu.co>
2025-05-26 13:51:58 -0500

    Correciones internacionalizacion

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-
+import type { Configuration } from 'webpack';
 const path = require('path');
 
-module.exports = {
-  webpack(config: any) {
+const nextConfig = {
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+  },
+  webpack(config: Configuration) {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = (config.resolve.alias || {}) as Record<string, string | false | string[]>;
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
 };
+
+module.exports = nextConfig;
